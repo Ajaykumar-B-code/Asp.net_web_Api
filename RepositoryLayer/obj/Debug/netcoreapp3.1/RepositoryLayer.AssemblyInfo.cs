32846ef1d9ab10f72eb919f93eb1319fdd364870
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-03-05 11:31:26 +0530

    [Ajay] Add : Adding Creation of Collab Api

--- RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5beef34ef41dc9389c57b3b361cbeab875c0e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c653fbe4f38e8ed446ef667074601c156eae37")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
